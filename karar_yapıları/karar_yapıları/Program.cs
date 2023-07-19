922f78d378d53e8802c4488b6261693e588293e4
Süleyman Samet <115584858+SuleymanSamet@users.noreply.github.com>
2023-07-19 08:24:56 +0300

    Add files via upload

---
+++ karar_yapıları/karar_yapıları/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour; //o anki saati alabiliriz
+        if (time>=6 &&time<11 )
+        {
+            Console.WriteLine("Günaydın.");
+        }
+        else if (time>=11 &&time<19)
+        {
+            Console.WriteLine("iyi günler");
+        }
+        else
+        {
+            Console.WriteLine("İyi geceler");
+        }
+
+        string sonuc =time <= 18 ? "iyigünler" : "iyi geceler";
+        sonuc = time>6 && time<11 ? "günaydın" : time<=18 ? "iyi günler":"iyi geceler";
+        Console.WriteLine(sonuc);
+    }
+}
