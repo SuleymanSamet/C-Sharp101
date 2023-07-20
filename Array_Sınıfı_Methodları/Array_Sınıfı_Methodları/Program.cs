4bdd009724a086bcc3a899d141517b5e4aa7543c
Süleyman Samet <115584858+SuleymanSamet@users.noreply.github.com>
2023-07-20 04:58:31 +0300

    Add files via upload

---
+++ Array_Sınıfı_Methodları/Array_Sınıfı_Methodları/Program.cs
@@ -0,0 +1,41 @@
+﻿//sort
+int[] sayiDizisi={23,12,86,72,3,11,17,4};
+Console.WriteLine("*******Sırasız oluşturulan dizi*********");
+foreach (var sayi in sayiDizisi)
+{
+    Console.WriteLine(sayi);
+}
+
+Console.WriteLine("*******Sıralı dizi*********");
+Array.Sort(sayiDizisi);
+foreach (var sayi in sayiDizisi)
+{
+    Console.WriteLine(sayi);
+}
+
+//clear 
+Console.WriteLine("*******Array Clear*********");
+Array.Clear(sayiDizisi,2,2); // 2 indexden başlayarak 2 tane elamana 0 değeri atadı
+foreach (var sayi in sayiDizisi)
+{
+    Console.WriteLine(sayi);
+}
+//reverse
+Console.WriteLine("***********tersine çevirme işlevi************");
+Array.Reverse(sayiDizisi);
+foreach (var sayi in sayiDizisi)
+{
+    Console.WriteLine(sayi);
+}
+//indexOf verilen elaman dizi içinde varsa indexsini döner
+Console.WriteLine("***********IndexOf sayı index bulma************");
+Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+//Ressize
+Console.WriteLine("***********ressize ile boyutlandırma************");
+Array.Resize<int>(ref sayiDizisi,9);
+sayiDizisi[8]=99; 
+foreach (var sayi in sayiDizisi)
+{
+    Console.WriteLine(sayi);
+}
