ed487a3e2093f7f0fabfaf709c1c7b72cfe476db
Süleyman Samet <115584858+SuleymanSamet@users.noreply.github.com>
2023-07-21 04:49:22 +0300

    Add files via upload

---
+++ metot_tanımlama/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 2;
+        int b = 3;
+
+        Console.WriteLine(a + b);
+
+        int sonuc = topla(a, b);
+        Console.WriteLine(sonuc);
+
+        Metotlar ornek = new Metotlar();
+        ornek.ekranayazdır(Convert.ToString(sonuc));
+        ornek.ekranayazdır(Convert.ToString(a+b));
+        int sonuc2 = ornek.arttırvetopla(a, b);
+         ornek.ekranayazdır(Convert.ToString(sonuc2));
+         ornek.ekranayazdır(Convert.ToString(a+b));
+    }
+
+    static int topla(int deger1, int deger2)
+    {
+        return deger1 + deger2;
+    }
+}
+
+class Metotlar
+{
+    public void ekranayazdır(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public int arttırvetopla(int deger1, int deger2)
+    {
+        deger1 += 1;
+        deger2 += 1;
+        return deger1 + deger2;
+    }
+}
